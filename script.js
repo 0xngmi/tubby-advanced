6e8365f17909d0d2481083a7b493c4ad00eed393
0xngmi <0xngmi@protonmail.com>
2022-02-22 12:52:05 +0000

    website

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getInstructions() {
+    try {
+        const proofs = await fetch("./proofs.json").then(r => r.json())
+        const address = document.getElementById("address").value.toLowerCase()
+        console.log(address)
+        if (address.length !== 42) {
+            alert("address is incorrect")
+            return
+        }
+        const proof = proofs[address]
+        if (proof === undefined) {
+            alert("your address is not in the whitelist")
+            return
+        }
+        if (proof === undefined) {
+            alert("your address is not in the whitelist")
+            return
+        }
+        const rugged = await fetch("./rugged.json").then(r => r.json())
+        if (rugged.find(add => add.toLowerCase() === address) !== undefined) {
+            alert("your address had an extra space at the end when submitted and cannot mint, we are working on a resolution")
+            return
+        }
+        document.getElementById("proof").innerText = "[" + proof.join(',') + "]"
+    } catch (e) {
+        alert(e)
+    }
+}
