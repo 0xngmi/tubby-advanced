01df4afd983d6ed6b699adc3dee1b497725ac0a0
0xngmi <0xngmi@protonmail.com>
2022-02-22 14:54:26 +0000

    add wl check and optimize

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
+const proofsPromise = fetch("./proofs.json").then(r => r.json())
+const ruggedPromise = fetch("./rugged.json").then(r => r.json())
+
+async function checkLists() {
+    const proofs = await proofsPromise
+    const address = document.getElementById("address").value.toLowerCase()
+    console.log(address)
+    if (address.length !== 42) {
+        alert("address is incorrect")
+        return null
+    }
+    const rugged = await ruggedPromise
+    if (rugged.find(add => add.toLowerCase() === address) !== undefined) {
+        alert("your address had an extra space at the end when submitted and cannot mint, we are working on a resolution")
+        return null
+    }
+    const proof = proofs[address]
+    if (proof === undefined) {
+        alert("your address is not in the whitelist")
+        return null
+    }
+    return proof
+}
+
 async function getInstructions() {
     try {
-        const proofs = await fetch("./proofs.json").then(r => r.json())
-        const address = document.getElementById("address").value.toLowerCase()
-        console.log(address)
-        if (address.length !== 42) {
-            alert("address is incorrect")
-            return
-        }
-        const rugged = await fetch("./rugged.json").then(r => r.json())
-        if (rugged.find(add => add.toLowerCase() === address) !== undefined) {
-            alert("your address had an extra space at the end when submitted and cannot mint, we are working on a resolution")
-            return
-        }
-        const proof = proofs[address]
-        if (proof === undefined) {
-            alert("your address is not in the whitelist")
+        const proof = await checkLists()
+        if(proof === null){
             return
         }
         document.getElementById("proof").innerText = "[" + proof.join(',') + "]"
+    } catch (e) {
+        alert(e)
+    }
+}
+
+async function checkWl() {
+    try {
+        const proof = await checkLists()
+        if(proof !== null){
+            alert("You are on the whitelist!")
+        }
     } catch (e) {
         alert(e)
     }
